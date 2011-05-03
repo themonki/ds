1e6fb404330fdc0a3a9cbf3c5d819353d2aed1a3
yerminson <yermigon@gmail.com>
2011-05-02 22:17:28 -0500

    fachadaBasica terminada

--- BibliotecaDigital/src/Utilidades/FachadaBD.java
+++ BibliotecaDigital/src/Utilidades/FachadaBD.java
@@ -1,5 +1,51 @@
 package Utilidades;
 
-public class FachadaBD {
 
-}
+/**
+ * Autor: Yerminson Gonzalez Munoz
+ * Responsabilidad: Realizar la debida conexion con la base de datos
+ * 
+ */
+import java.sql.*;
+public class FachadaBD {
+        String url, usuario, password;
+        Connection conexion;
+        Statement instruccion;
+        ResultSet tabla;
+        FachadaBD(){
+            url="jdbc:postgresql://localhost:5432/proyecto";
+            usuario="yerdogm";
+            password="yerdogm";
+        }
+
+        public Connection conectar(){
+            try {
+            // Se carga el driver
+            Class.forName("org.postgresql.Driver");
+            System.out.println( "Driver Cargado" );
+            } catch( Exception e ) {
+                System.out.println( "No se pudo cargar el driver." );
+            }
+
+            try{
+                     //Crear el objeto de conexion a la base de datos
+                     conexion = DriverManager.getConnection(url, usuario, password);
+                     System.out.println( "Conexion Abierta" );
+                     return conexion;
+                 
+             } catch( Exception e ) {
+                System.out.println( "No se pudo abrir." );
+                return null;
+             }
+
+        }//end connectar
+
+        public void cerrarConexion(Connection c){
+            try{
+                 c.close();
+                 System.out.println( "Cerrada la conexion a la base de datos" );
+            } catch( Exception e ) {
+                System.out.println( "No se pudo cerrar la BD" );
+            }
+        }
+}//end class
