025db8d73b372d61e056f8bead1c3a57a9e5e237
yerminson <yermigon@gmail.com>
2011-05-03 01:29:50 -0500

    clase autor logica

--- BibliotecaDigital/src/GestionDocumento/Logica/Autor.java
+++ BibliotecaDigital/src/GestionDocumento/Logica/Autor.java
@@ -1,5 +1,22 @@
 package GestionDocumento.Logica;
-
+/*
+ * Nombre: Yerminson Gonzalez Munoz
+ * Responsabilidad: Clase que respresenta un autor.
+ * 
+ * */
 public class Autor {
+	
+	String nombre;
+	String apellido;
+	String acronimo;
+	String correo;
+	String id;
+	
+	String getNombre(){return nombre;}
+	String apellido(){return apellido;}
+	String getAcronimo(){return acronimo;}
+	String getCorreo(){return correo;}
+	String getId(){ return id;}
+	
 
 }
