dd21be743d381015a2676c58854f42508f0dc5ff
yerminson <yermigon@gmail.com>
2011-05-02 23:57:59 -0500

    documento modificado full :D

--- BibliotecaDigital/src/Documento/Logica/Documento.java
+++ BibliotecaDigital/src/Documento/Logica/Documento.java
@@ -1,70 +1,74 @@
 package Documento.Logica;
 
 import java.util.Vector;
+import GestionDocumento.Logica.AreaConocimiento;
+import GestionDocumento.Logica.Autor;
+import GestionDocumento.Logica.PalabraClave;
 
 public class Documento {
 	
-	String id_doc;
-	String titulo_secundario;
-	String tipoMaterial;
-	String fechaDeCatalogacion;
-	String tituloppal;
-	String derechosDeAutor;
-	String editorial;
+		
+	String idDocumento;
 	String idioma;
-	String tamano;
-	String url;	
-	String software_recomendado;
-	String resolucion;
-	String formato;
-	String resumen;
-	String catalogadorLogin;
+	String derechosDeAutor;
 	String descripcion;
-	String fecha_creacion;
-	String fecha_publicacion;
-	String fechaDeCatalogacion;
-	Vector autores;
-	Vector areas;
-	Vector palabrasClave;
+	String softwareRecomendado;
+	String resolucion;
+	String editorial;
+	String formato;	
+	String tituloPrincipal;	
+	String tituloSecundario;
+	String link;	
+	String fechaCreacion;
+	String fechaPublicacion;
+	String fechaCatalogacion;
+	String loginCatalogador;
+	String tipoMaterial;		
+	Vector<Autor> autores;
+	Vector<AreaConocimiento> areas;
+	Vector<PalabraClave> palabrasClave;
 	
-	public String getid_doc(){ return this.id_doc;}
+	
+	
+	
+	public String getid_doc(){ return this.idDocumento;}
 	public String getIdioma(){ return this.idioma;}
 	public String getderechosDeAutor(){return this.derechosDeAutor;}
 	public String getDescripcion(){return this.descripcion;}
-	public String getSoftware_recomentado(){return this.software_recomendado;}
+	public String getSoftware_recomentado(){return this.softwareRecomendado;}
 	public String getResolucion(){return this.resolucion;}
 	public String getEditorial(){return this.editorial;}
 	public String getFormato(){return this.formato;}
-	public String gettituloppal(){return this.tituloppal;}
-	public String getTitulo_secundario(){return this.titulo_secundario;}
-	public String geturl(){return this.url;}
-	public String getFecha_creacion(){return this.fecha_creacion;}
-	public String getFecha_publicacion(){return this.fecha_publicacion;}
-	public String getfechaDeCatalogacion(){return this.fechaDeCatalogacion;}
-	public String getcatalogadorLogin(){return this.catalogadorLogin;}
+	public String gettituloppal(){return this.tituloPrincipal;}
+	public String getTitulo_secundario(){return this.tituloSecundario;}
+	public String geturl(){return this.link;}
+	public String getFecha_creacion(){return this.fechaCreacion;}
+	public String getFecha_publicacion(){return this.fechaPublicacion;}
+	public String getfechaDeCatalogacion(){return this.fechaCatalogacion;}
+	public String getcatalogadorLogin(){return this.loginCatalogador;}
 	public String getTipoMaterial(){return this.tipoMaterial;}
-	public Vector getAutores(){return this.autores;}
-	public Vector getAreas(){return this.areas;}
-	public Vector getPalabrasClave(){return this.palabrasClave;}
+	public Vector<Autor> getAutores(){return this.autores;}
+	public Vector<AreaConocimiento> getAreas(){return this.areas;}
+	public Vector<PalabraClave> getPalabrasClave(){return this.palabrasClave;}
 	
-	public void getid_doc(String value){ id_doc= value;}
+	public void getid_doc(String value){ idDocumento = value;}
 	public void getIdioma(String value){ idioma= value;}
 	public void getderechosDeAutor(String value){derechosDeAutor= value;}
 	public void getDescripcion(String value){descripcion= value;}
-	public void getSoftware_recomentado(String value){software_recomendado= value;}
+	public void getSoftware_recomentado(String value){softwareRecomendado= value;}
 	public void getResolucion(String value){resolucion= value;}
 	public void getEditorial(String value){editorial= value;}
 	public void getFormato(String value){formato= value;}
-	public void gettituloppal(String value){tituloppal= value;}
-	public void getTitulo_secundario(String value){titulo_secundario= value;}
-	public void geturl(String value){url= value;}
-	public void getFecha_creacion(String value){fecha_creacion= value;}
-	public void getFecha_publicacion(String value){fecha_publicacion= value;}
-	public void getfechaDeCatalogacion(String value){fechaDeCatalogacion= value;}
-	public void getcatalogadorLogin(String value){catalogadorLogin= value;}
+	public void gettituloppal(String value){tituloPrincipal= value;}
+	public void getTitulo_secundario(String value){tituloSecundario= value;}
+	public void geturl(String value){link= value;}
+	public void getFecha_creacion(String value){fechaCreacion= value;}
+	public void getFecha_publicacion(String value){fechaPublicacion= value;}
+	public void getfechaDeCatalogacion(String value){fechaCatalogacion= value;}
+	public void getcatalogadorLogin(String value){loginCatalogador= value;}
 	public void getTipoMaterial(String value){tipoMaterial= value;}
-	public void getAutores(Vector value){autores= value;}
-	public void getAreas(Vector value){areas= value;}
-	public void getPalabrasClave(Vector value){palabrasClave= value;}
+	public void getAutores(Vector<Autor> value){autores= value;}
+	public void getAreas(Vector<AreaConocimiento> value){areas= value;}
+	public void getPalabrasClave(Vector<PalabraClave> value){palabrasClave= value;}
 
 }
