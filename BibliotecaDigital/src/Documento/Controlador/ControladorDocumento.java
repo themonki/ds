515122a4a8948d2f052ec64fda444f01acda4f67
themonki <edgarandres29@gmail.com>
2011-05-04 00:14:36 -0500

    adicion de los metodos de controlador modificar para dao y controlador en documento

--- BibliotecaDigital/src/Documento/Controlador/ControladorDocumento.java
+++ BibliotecaDigital/src/Documento/Controlador/ControladorDocumento.java
@@ -11,8 +11,7 @@ public class ControladorDocumento {
 			String titulo_principal, String titulo_secundario, String link, String creacion, 
 			String publicacion, String catalogacion, String login){
         Documento d = new Documento();
-        DaoDocumento daoDoc=new DaoDocumento();
-
+        
         d.setId_doc(id);
         d.setIdioma(idioma);
         d.setDerechosDeAutor(derechos);
@@ -29,17 +28,56 @@ public class ControladorDocumento {
         d.setFechaDeCatalogacion(catalogacion);
         d.setCatalogadorLogin(login);
         
-        //Se llama al dao para guardar
-        //System.out.println("Se va a insertar un programa");
-        
-        daoDoc.guardarDocumento(d);
+        insertarDocumento(d);
 
-        System.out.println("Se inserto el documento");
+        //por seguridad
+        d=null;       
+
+    }
+	
+	public void insertarDocumento(Documento d){
+		DaoDocumento daoDoc=new DaoDocumento();
+		daoDoc.guardarDocumento(d);
+		
+		System.out.println("Se inserto el documento");
+		daoDoc=null;
+		
+	}
+	
+	public void modificarDocumento(String id, String idioma, String derechos, String descripcion, 
+			String software, String resolucion, String editorial, String formato, 
+			String titulo_principal, String titulo_secundario, String link, String creacion, 
+			String publicacion, String catalogacion, String login){
+		Documento d = new Documento();
+        
+        d.setId_doc(id);
+        d.setIdioma(idioma);
+        d.setDerechosDeAutor(derechos);
+        d.setDescripcion(descripcion);
+        d.setSoftware_recomentado(software);
+        d.setResolucion(resolucion);
+        d.setEditorial(editorial);
+        d.setFormato(formato);
+        d.setTituloppal(titulo_principal);
+        d.setTitulo_secundario(titulo_secundario);
+        d.setUrl(link);
+        d.setFecha_creacion(creacion);
+        d.setFecha_publicacion(publicacion);
+        d.setFechaDeCatalogacion(catalogacion);
+        d.setCatalogadorLogin(login);
+        
+        modificarDocumento(d);
 
         //por seguridad
         d=null;
-        daoDoc=null;
+	}
 
-    }
-
+	public void modificarDocumento(Documento d){
+		DaoDocumento daoDoc=new DaoDocumento();
+		daoDoc.modificarDocumento(d);
+		
+		System.out.println("Se inserto el documento");
+		daoDoc=null;
+		
+	}
 }
