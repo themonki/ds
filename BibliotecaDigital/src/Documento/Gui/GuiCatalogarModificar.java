33e65895b764ace781d3ac4fad3b3f1bd6ab2d55
felipex <felipex1991@gmail.com>
2011-05-03 20:57:04 -0500

    mejoras de gui catalogar

--- BibliotecaDigital/src/Documento/Gui/GuiCatalogarModificar.java
+++ BibliotecaDigital/src/Documento/Gui/GuiCatalogarModificar.java
@@ -1,4 +1,4 @@
-package Documento.Gui;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
@@ -12,137 +12,129 @@ import java.awt.Graphics2D;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.Insets;
 import java.awt.Label;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.sql.Date;
-import  javax.swing.*;
+import java.util.Vector;
 
-@SuppressWarnings({ "serial", "unused" })
+import  javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+
 public class GuiCatalogarModificar extends JFrame{
 	
 	 
-	JPanel panel,panel2,panel3,panel4,panel5,panel6,panel7,panel8,panel9;
+	JPanel panel,panel2,panel3,panel4,panel5,panelConAutores,panelConpalabrasC,panel8,panelConAreas;
 	JLabel tipoMaterial,numeroIndentificacion,tituloPrincipal,idioma,autor,
 	tituloSecundario,traducido,editorial,derechosAutor,descripcion,indicacion,
-	palabrasClave,fechaPublicacion;
+	palabrasClave,fechaPublicacion,areas;
 	
 	JTextArea campoDescripcion;
 
 	JComboBox campoPalabras,campoAutor,campoTipoMaterial,
-	campoTraducido,campoIdioma,campoDerechosAutor;
+	campoTraducido,campoIdioma,campoDerechosAutor,campoAreas;
 	  
 	JTextField campoEditorial,campoNumeroIdentificacion,campoTituloSecundario,campoTituloPpal;
 	
 	JButton botonCatalogar,nuevaArea,nuevotipo,nuevoAutor,nuevoidioma,nuevapalabra;
 	//faltan las fechas /////////****************///
-    // en caccoo falta campo editorial
+  // en caccoo falta campo editorial
 	
-
+	Vector<String> palabrasClaveVec,areasVector,autoresVector,
+	       palabActualVec,areasActualVecr,autoresActualVector;
+	
+	
 	public GuiCatalogarModificar() {
 		
 		initComponents();
 	}
 
 	public void initComponents(){
+		
 		super.setTitle("Catalogar Documento");
-		super.setIconImage(new ImageIcon("LOGO.png").getImage() );
+		super.setIconImage(new ImageIcon("LOGO1.png").getImage() );
+	    indicacion = new JLabel("   Catalogar Documento");
 		
 	
+		//---------------vectores para los ComboBox------------------
+		areasVector= new Vector<String>();
+		autoresVector= new Vector<String>();
+		palabrasClaveVec= new Vector<String>();
+		//---------------consulta en sql----------------------
+		palabrasClaveVec.add("Software");
+		palabrasClaveVec.add("linux");
+		palabrasClaveVec.add("word");
+		autoresVector.add("juan grabriel");
+		autoresVector.add("andres juan ");
+		autoresVector.add("Leopoldo");
+		areasVector.add("ingles");
+		areasVector.add("astronomia");
+		areasVector.add("nadaderia");
+		//----------------------------------------
+		palabActualVec= new Vector<String>();
+		areasActualVecr= new Vector<String>();
+		autoresActualVector= new Vector<String>();
+		//----------------------------------------	
 		panel= new JPanel();
 		panel2= new JPanel();
 		panel3= new JPanel();
 		panel4= new JPanel();
 		panel5= new JPanel();
-		panel6= new JPanel();
-		panel7= new JPanel();
+		panelConAutores= new JPanel();
+		panelConpalabrasC= new JPanel();
 		panel8= new JPanel();
-		panel9= new JPanel();
-		
+		panelConAreas= new JPanel();
+		//-------------------------------fuentes letras-------------------------
 		Font font1 = new Font("Book Antiqua",Font.BOLD+ Font.ITALIC, 17);
 		Font font2 = new Font("Book Antiqua",Font.BOLD, 15);
+		Font font3 = new Font("Book Antiqua",Font.BOLD+ Font.ITALIC, 25);
 		
 		//----------------------------------------------------
-		tipoMaterial= new JLabel(" Tipo De Material:",JLabel.LEFT);
-		tituloPrincipal= new JLabel(" Titulo Principal:",JLabel.LEFT);
-		idioma= new JLabel(" Idioma:",JLabel.LEFT);
-	    tituloSecundario= new JLabel(" Titulo Secundario:",JLabel.LEFT);
-	    traducido= new JLabel(" Traducido a :",JLabel.LEFT);
-	    editorial= new JLabel(" Editorial:",JLabel.LEFT);
-	    derechosAutor= new JLabel(" Derechos De Autor:",JLabel.LEFT);
-	    descripcion= new JLabel(" Descripcion:",JLabel.CENTER);
-	    autor = new JLabel(" Autor:");
-	    indicacion = new JLabel(" ");
-	    palabrasClave= new JLabel(" Palabras Clave:");
-	    fechaPublicacion= new JLabel("Fecha De Publicacion");//&&&&&&
-		JLabel 
-		autores= new JLabel("Lista De Autores Actual.",JLabel.CENTER),
-		palabrasC= new JLabel("Palabras Clave Actuales."),
-		areaPertenece= new JLabel("Areas Actuales.");
+		inicializarLabels(font1);
+		//--------------------------------------------------
+		inicializarComboBox(font2);	
+		//------------------------------------------------------------
+		inicializarTexfield();
+		///--------------------------------------------------
+		inicializarButton();
 		
-		
-		
-	    campoDescripcion= new JTextArea();
-	    campoTraducido= new JComboBox();
-	    campoIdioma= new JComboBox();
-	    campoDerechosAutor= new JComboBox();
-	    campoTipoMaterial= new JComboBox();
-	    campoAutor = new JComboBox();
-	    campoPalabras= new JComboBox();
-	    
-	    campoNumeroIdentificacion = new JTextField();
-	    campoTituloSecundario= new JTextField();
-	    campoTituloPpal = new JTextField();
-	    campoEditorial= new JTextField();
-	    
-	    campoDescripcion= new JTextArea(5,30);
-	    
-	    botonCatalogar= new JButton("FINALIZAR"); 
-	    nuevaArea= new JButton("Crear Area");
-	    nuevotipo= new JButton("Crear Tipo");
-	    nuevoAutor = new JButton("Crear Autor");
-	    nuevapalabra= new JButton("Crear Palabra");
-	    
-	    Color colorletras= new Color(0,70,0);
-	    
-	    palabrasClave.setFont(font1);
-	    autor.setFont(font1);
-	    descripcion.setFont(font1);
-		tipoMaterial.setFont(font1);
-		tituloPrincipal.setFont(font1);
-		tituloSecundario.setFont(font1);
-		idioma.setFont(font1);
-		traducido.setFont(font1);
-		editorial.setFont(font1);
-		derechosAutor.setFont(font1);
-		
-		palabrasClave.setForeground(colorletras);
-	    autor.setForeground(colorletras);
-	    descripcion.setForeground(colorletras);
-		tipoMaterial.setForeground(colorletras);
-		tituloPrincipal.setForeground(colorletras);
-		tituloSecundario.setForeground(colorletras);
-		idioma.setForeground(colorletras);
-		traducido.setForeground(colorletras);
-		editorial.setForeground(colorletras);
-		derechosAutor.setForeground(colorletras);
-		//-------------------------------
+	   
+	    //-----------labels locales-----------------------
+	    JLabel 
+		autores= new JLabel("    Lista De Autores Actual.       ",JLabel.CENTER),
+		palabrasC= new JLabel("      Palabras Clave Actuales.     "),
+		areaPertenece= new JLabel("     Areas Actuales.    ");
+	    indicacion = new JLabel("   Catalogar Documento",JLabel.CENTER);
+		Color colorletras= new Color(0,50,10);
 		areaPertenece.setFont(font2);
 		palabrasC.setFont(font2);
 		autores.setFont(font2);
+		indicacion.setFont(font3);
 		areaPertenece.setForeground(colorletras);
 		palabrasC.setForeground(colorletras);
 		autores.setForeground(colorletras);
-		//panel2.setLayout(new GridLayout(5,2,10,10));	
+		indicacion.setForeground(new Color(0,50,0));
+		indicacion.setBackground(Color.red);
+		
+		
+	    //-----------------------------ponerBordeaPanel----------	
+		
 		panel5.setBorder(BorderFactory.createLineBorder(Color.yellow));
 		panel4.setBorder(BorderFactory.createLineBorder(Color.yellow));
 		panel2.setBorder(BorderFactory.createLineBorder(Color.yellow));
-		panel6.setBorder(BorderFactory.createLineBorder(Color.yellow));
-		panel7.setBorder(BorderFactory.createLineBorder(Color.yellow));
-		panel9.setBorder(BorderFactory.createLineBorder(Color.yellow));
+		panelConAutores.setBorder(BorderFactory.createLineBorder(Color.yellow));
+		panelConpalabrasC.setBorder(BorderFactory.createLineBorder(Color.yellow));
+		panelConAreas.setBorder(BorderFactory.createLineBorder(Color.yellow));
 
 		
-		
+		//Organizacion de layouts y paneles
 		panel2.setLayout(new GridBagLayout());
 		panel3.setLayout(new FlowLayout());
 		panel4.setLayout(new BorderLayout());
@@ -152,15 +144,15 @@ public class GuiCatalogarModificar extends JFrame{
 		,restriccionEtiquetas= new GridBagConstraints(),
 		restriccionBotones= new GridBagConstraints();
 		
-		restriccionCampo.ipadx = 340;      		
+		restriccionCampo.ipadx = 360;      		
 		restriccionCampo.weightx = 10.0;
 		restriccionCampo.gridwidth = 2;
 		restriccionCampo.gridx = 1;
 		restriccionCampo.gridy = 0;
-		restriccionCampo.insets = new Insets(10,40,10,10);
+		restriccionCampo.insets = new Insets(5,40,5,0);
 		
 		
-		restriccionEtiquetas.insets= new Insets(0,40,10,10);// espacios entre componentes
+		restriccionEtiquetas.insets= new Insets(0,14,0,0);// espacios entre componentes
 		restriccionEtiquetas.anchor=GridBagConstraints.WEST;//alinear a la izquierda
 		restriccionCampo.anchor=GridBagConstraints.WEST;
 		restriccionEtiquetas.gridy=0;
@@ -178,9 +170,11 @@ public class GuiCatalogarModificar extends JFrame{
 		restriccionCampo.gridy = 2;
 		restriccionCampo.ipadx=140;
 		restriccionCampo.gridwidth=1;
+
 		restriccionBotones.gridx=2;
 		restriccionBotones.gridy=2;
 		restriccionBotones.ipadx=20;
+		
 		panel2.add(autor,restriccionEtiquetas);
 		panel2.add(campoAutor,restriccionCampo);		
 		panel2.add(nuevoAutor,restriccionBotones);
@@ -188,7 +182,6 @@ public class GuiCatalogarModificar extends JFrame{
 		restriccionEtiquetas.gridy=3;
 		restriccionCampo.gridy = 3;
 		restriccionCampo.ipadx=90;
-		restriccionCampo.gridwidth=2;
 		
 		panel2.add(idioma,restriccionEtiquetas);
 		panel2.add(campoIdioma,restriccionCampo);
@@ -202,10 +195,13 @@ public class GuiCatalogarModificar extends JFrame{
 		
 		restriccionEtiquetas.gridy=5;
 		restriccionCampo.gridy = 5;
-		restriccionCampo.ipadx=110;
+		restriccionCampo.ipadx=100;
 		restriccionCampo.gridwidth=1;
-		restriccionBotones.gridx=2;
 		restriccionBotones.gridy=5;
+		restriccionBotones.anchor= GridBagConstraints.EAST;
+		restriccionBotones.gridx=1;
+		restriccionBotones.ipadx=10;
+		
 		panel2.add(tipoMaterial,restriccionEtiquetas);
 		panel2.add(campoTipoMaterial,restriccionCampo);
 		panel2.add(nuevotipo,restriccionBotones);
@@ -214,23 +210,39 @@ public class GuiCatalogarModificar extends JFrame{
 		restriccionCampo.gridy = 6;
 		restriccionCampo.ipadx=200;
 		restriccionCampo.gridwidth=2;
+		
 		panel2.add(editorial,restriccionEtiquetas);
 		panel2.add(campoEditorial,restriccionCampo);
 		
 		restriccionEtiquetas.gridy=7;
 		restriccionCampo.gridy = 7;
-		restriccionCampo.ipadx=120;
+		restriccionCampo.ipadx=10;
 		restriccionCampo.gridwidth=1;
 		restriccionBotones.gridy=7;
+		restriccionBotones.ipadx=7;
+		restriccionBotones.gridx=1;
+		
+		
 		panel2.add(palabrasClave,restriccionEtiquetas);
 		panel2.add(campoPalabras,restriccionCampo);
 		panel2.add(nuevapalabra,restriccionBotones);
 		
 		restriccionEtiquetas.gridy=8;
 		restriccionCampo.gridy = 8;
+		
 		panel2.add(derechosAutor,restriccionEtiquetas);
 		panel2.add(campoDerechosAutor,restriccionCampo);
-
+		
+		restriccionEtiquetas.gridy=9;
+		restriccionCampo.gridy = 9;
+		restriccionCampo.ipadx=150;
+		restriccionBotones.gridy=9;
+		restriccionBotones.ipadx=9;
+		restriccionBotones.gridx=2;
+	
+		panel2.add(areas,restriccionEtiquetas);
+		panel2.add(campoAreas,restriccionCampo);
+		panel2.add(nuevaArea,restriccionBotones);
 		
 		panel3.add(panel2);
 		JScrollPane scroll = new JScrollPane(campoDescripcion);
@@ -243,15 +255,12 @@ public class GuiCatalogarModificar extends JFrame{
 		panel4.add(panel5,BorderLayout.CENTER);
 		panel4.add(panel,BorderLayout.SOUTH);
 		
-	
-		//panel6.setLayout(new GridLayout(6,1,10,10));
-		
-		panel6.add(autores); 
-		panel7.add(palabrasC);
-		panel9.add(areaPertenece);
-		panel8.add(panel6);
-		panel8.add(panel7);
-		panel8.add(panel9);
+		panelConAutores.add(autores); 
+		panelConpalabrasC.add(palabrasC);
+		panelConAreas.add(areaPertenece);
+		panel8.add(panelConAutores);
+		panel8.add(panelConpalabrasC);
+		panel8.add(panelConAreas);
 
 		
 		setLayout(new BorderLayout());
@@ -262,19 +271,117 @@ public class GuiCatalogarModificar extends JFrame{
 		//------------------------------------------
 		setVisible(true);
 		setResizable(true);
-		setSize(900,620);
+		setSize(900,670);
 		//-------------------------------------------
 	
 		
 		
 	}
 	
+	private void inicializarButton() {
+		botonCatalogar= new JButton("   FINALIZAR   "); 
+	    nuevaArea= new JButton("Crear Area");
+	    nuevotipo= new JButton("Crear Tipo");
+	    nuevoAutor = new JButton("Crear Autor");
+	    nuevapalabra= new JButton("Crear Palabra");
+	}
+
+	private void inicializarTexfield() { 
+		campoNumeroIdentificacion = new JTextField();
+		campoTituloSecundario= new JTextField();    
+		campoTituloPpal = new JTextField();   
+		campoEditorial= new JTextField();
+		campoDescripcion= new JTextArea(5,30);
+		
+	}
+
+	private void inicializarLabels(Font font1) 
+	{
+		
+		tipoMaterial= new JLabel(" Tipo De Material:",JLabel.LEFT);
+		tituloPrincipal= new JLabel(" Titulo Principal:",JLabel.LEFT);
+		idioma= new JLabel(" Idioma:",JLabel.LEFT);
+	    tituloSecundario= new JLabel(" Titulo Secundario:",JLabel.LEFT);
+	    traducido= new JLabel(" Traducido a :",JLabel.LEFT);
+	    editorial= new JLabel(" Editorial:",JLabel.LEFT);
+	    derechosAutor= new JLabel(" Derechos De Autor:",JLabel.LEFT);
+	    descripcion= new JLabel(" Descripcion:",JLabel.CENTER);
+	    autor = new JLabel(" Autor:");
+	
+	    palabrasClave= new JLabel(" Palabras Clave:");
+	    fechaPublicacion= new JLabel("Fecha De Publicacion");//&&&&&&
+	    areas= new JLabel("Areas :");
+	    
+ 
+	    Color colorletras= new Color(0,60,0);
+	    
+	    palabrasClave.setFont(font1);
+	    autor.setFont(font1);
+	    descripcion.setFont(font1);
+		tipoMaterial.setFont(font1);
+		tituloPrincipal.setFont(font1);
+		tituloSecundario.setFont(font1);
+		idioma.setFont(font1);
+		traducido.setFont(font1);
+		editorial.setFont(font1);
+		derechosAutor.setFont(font1);
+		areas.setFont(font1);
+		
+		indicacion.setFont(font1);
+		
+		palabrasClave.setForeground(colorletras);
+	    autor.setForeground(colorletras);
+	    descripcion.setForeground(colorletras);
+		tipoMaterial.setForeground(colorletras);
+		tituloPrincipal.setForeground(colorletras);
+		tituloSecundario.setForeground(colorletras);
+		idioma.setForeground(colorletras);
+		traducido.setForeground(colorletras);
+		editorial.setForeground(colorletras);
+		derechosAutor.setForeground(colorletras);
+		areas.setForeground(colorletras);		
+	}
+
+	private void inicializarComboBox(Font font2) 
+	{
+		campoAreas= new JComboBox(areasVector);
+	    campoDescripcion= new JTextArea();
+	    campoTraducido= new JComboBox();
+	    campoIdioma= new JComboBox();
+	    campoDerechosAutor= new JComboBox();
+	    campoTipoMaterial= new JComboBox();
+	    campoAutor = new JComboBox(autoresVector);
+	    campoPalabras= new JComboBox(palabrasClaveVec);	  
+	    //-------------------font combobox--------------------------
+	    campoPalabras.setFont(font2);
+	    campoAreas.setFont(font2);
+	    campoDescripcion.setFont(font2);
+	    campoTraducido.setFont(font2);
+	    campoIdioma.setFont(font2);
+	    campoDerechosAutor.setFont(font2);
+	    campoTipoMaterial.setFont(font2);
+	    campoAreas.setFont(font2);
+	    campoAutor.setFont(font2);
+	    campoPalabras.setFont(font2);
+	    //---------------------------------------------------------
+	    campoAutor.addActionListener(new ManejadorComboBox());
+	    campoPalabras.addActionListener(new ManejadorComboBox());    
+	    campoAreas.addActionListener(new ManejadorComboBox());
+	    
+	}
+
 	public static void main (String args []){
-		try{
+		
+		try
+		{
+			
 			
 			UIManager.setLookAndFeel("com.nilo.plaf.nimrod.NimRODLookAndFeel"); 
 			
-		}catch (Exception e){e.printStackTrace();}
+		
+		}
+		catch (Exception e){e.printStackTrace();}
+		
 		GuiCatalogarModificar ventana;
 		ventana = new GuiCatalogarModificar();
 		ventana.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
@@ -282,5 +389,130 @@ public class GuiCatalogarModificar extends JFrame{
 	}
 	
 	
+	
+	 
+	private class ManejadorComboBox implements ActionListener {
 
+		//int panel;
+		//ManejadorComboBox(int i){panel = i;};
+		public void actionPerformed(ActionEvent event) {
+			
+
+			JLabel etiqueta= new JLabel();
+			
+			if (event.getSource()==campoAutor)
+			{
+				
+				if (autoresActualVector.indexOf(campoAutor.getSelectedItem())==-1)
+				{
+					autoresActualVector.add((String) campoAutor.getSelectedItem());		
+					etiqueta.setText(""+campoAutor.getSelectedItem());			
+					etiqueta.addMouseListener(new eventoMouse(1));			
+					panelConAutores.add(etiqueta);			
+					panelConAutores.updateUI();
+				}
+				
+			};
+			if (event.getSource()== campoPalabras)
+			
+			{	
+			
+				if (palabActualVec.indexOf(campoPalabras.getSelectedItem())==-1)
+				{
+					palabActualVec.add((String) campoPalabras.getSelectedItem());		
+					etiqueta.setText(""+campoPalabras.getSelectedItem());			
+					etiqueta.addMouseListener(new eventoMouse(2));			
+					panelConpalabrasC.add(etiqueta);			
+					panelConpalabrasC.updateUI();
+				}
+		
+				
+			}
+
+			if (event.getSource()==campoAreas)
+			{
+				if (areasActualVecr.indexOf(campoAreas.getSelectedItem())==-1)
+				{
+					
+					areasActualVecr.add((String) campoAreas.getSelectedItem());			
+					etiqueta.setText(""+campoAreas.getSelectedItem());				
+					etiqueta.addMouseListener(new eventoMouse(3));			
+					panelConAreas.add(etiqueta);			
+					panelConAreas.updateUI();
+				}
+				
+			
+			};
+			
+		
+		}
+		
+		private class eventoMouse implements MouseListener {
+
+			int panel=0;//usado para saber de que panel es la palabra autor area o palabraclave
+			
+			eventoMouse (int i ){panel = i;};
+			public void mouseClicked(MouseEvent arg0) {
+
+				
+				if (panel== 1)
+				{
+				
+					JLabel refe=(JLabel) arg0.getSource();			
+					autoresActualVector.removeElement(refe.getText());																		
+					panelConAutores.remove(refe);									
+					panelConAutores.updateUI();				
+				}
+				
+				if (panel == 2 )
+				{
+					
+					JLabel refe=(JLabel) arg0.getSource();
+											
+					palabActualVec.removeElement(refe.getText());														
+					panelConpalabrasC.remove(refe);					
+					panelConpalabrasC.updateUI();
+					}
+				
+				if (panel == 3){
+					JLabel refe=(JLabel) arg0.getSource();
+											
+					areasActualVecr.removeElement(refe.getText());														
+					panelConAreas.remove(refe);					
+					panelConAreas.updateUI();
+					}
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+			
+				JLabel refe=(JLabel) arg0.getSource();
+				refe.setForeground(Color.red);
+				refe.setIcon(new ImageIcon("CRUZ.gif"));
+				refe.updateUI();
+				
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				
+				JLabel refe=(JLabel) arg0.getSource();
+				refe.setForeground(Color.black);
+				refe.setIcon(new ImageIcon(""));				
+								
+			}
+			
+			public void mousePressed(MouseEvent arg0) {}
+			public void mouseReleased(MouseEvent arg0){}
+
+		
+		}
+
+			
+		
+
+	
+	
+	}
 }
