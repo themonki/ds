0ac1cc1a860c696c3f1d2447e1ee93e6dfaf5a00
themonki <edgarandres29@gmail.com>
2011-05-03 22:29:28 -0500

    DaoDocumento con contructor y metodo de insertar no implementado y correccion de metodos set documento

--- BibliotecaDigital/src/Documento/Dao/DaoDocumento.java
+++ BibliotecaDigital/src/Documento/Dao/DaoDocumento.java
@@ -1,5 +1,25 @@
 package Documento.Dao;
 
+import java.sql.*;
+import Utilidades.FachadaBD;
+import Documento.Logica.*;
 public class DaoDocumento {
+	
+	FachadaBD fachada;
+	
+	DaoDocumento(){
+		fachada = new FachadaBD();
+	}
+	
+	public void guardarDocumento(String id, String idioma, String derechos, String descripcion,
+			String software,String resolucion,String editorial ,String formato,String titulo_principal,
+			String titulo_secundario,String link, ,String creacion,String publicacion,String catalogacion,
+			String login){
+		
+	
+	}
+	
+	public void guardarDocumento(Documento d){}
+	
 
 }
--- BibliotecaDigital/src/Documento/Logica/Documento.java
+++ BibliotecaDigital/src/Documento/Logica/Documento.java
@@ -59,43 +59,43 @@ public class Documento {
 		this.palabrasClave = palabrasClave;
 	}	
 	
-	public String getid_doc(){ return this.idDocumento;}
+	public String getId_doc(){ return this.idDocumento;}
 	public String getIdioma(){ return this.idioma;}
-	public String getderechosDeAutor(){return this.derechosDeAutor;}
+	public String getDerechosDeAutor(){return this.derechosDeAutor;}
 	public String getDescripcion(){return this.descripcion;}
 	public String getSoftware_recomentado(){return this.softwareRecomendado;}
 	public String getResolucion(){return this.resolucion;}
 	public String getEditorial(){return this.editorial;}
 	public String getFormato(){return this.formato;}
-	public String gettituloppal(){return this.tituloPrincipal;}
+	public String getTituloppal(){return this.tituloPrincipal;}
 	public String getTitulo_secundario(){return this.tituloSecundario;}
-	public String geturl(){return this.link;}
+	public String getUrl(){return this.link;}
 	public String getFecha_creacion(){return this.fechaCreacion;}
 	public String getFecha_publicacion(){return this.fechaPublicacion;}
-	public String getfechaDeCatalogacion(){return this.fechaCatalogacion;}
-	public String getcatalogadorLogin(){return this.loginCatalogador;}
+	public String getFechaDeCatalogacion(){return this.fechaCatalogacion;}
+	public String getCatalogadorLogin(){return this.loginCatalogador;}
 	public String getTipoMaterial(){return this.tipoMaterial;}
 	public Vector<Autor> getAutores(){return this.autores;}
 	public Vector<AreaConocimiento> getAreas(){return this.areas;}
 	public Vector<PalabraClave> getPalabrasClave(){return this.palabrasClave;}	
-	public void getid_doc(String value){ idDocumento = value;}
-	public void getIdioma(String value){ idioma= value;}
-	public void getderechosDeAutor(String value){derechosDeAutor= value;}
-	public void getDescripcion(String value){descripcion= value;}
-	public void getSoftware_recomentado(String value){softwareRecomendado= value;}
-	public void getResolucion(String value){resolucion= value;}
-	public void getEditorial(String value){editorial= value;}
-	public void getFormato(String value){formato= value;}
-	public void gettituloppal(String value){tituloPrincipal= value;}
-	public void getTitulo_secundario(String value){tituloSecundario= value;}
-	public void geturl(String value){link= value;}
-	public void getFecha_creacion(String value){fechaCreacion= value;}
-	public void getFecha_publicacion(String value){fechaPublicacion= value;}
-	public void getfechaDeCatalogacion(String value){fechaCatalogacion= value;}	
-	public void getcatalogadorLogin(String value){loginCatalogador= value;}
-	public void getTipoMaterial(String value){tipoMaterial= value;}
-	public void getAutores(Vector<Autor> value){autores= value;}
-	public void getAreas(Vector<AreaConocimiento> value){areas= value;}
-	public void getPalabrasClave(Vector<PalabraClave> value){palabrasClave= value;}
+	public void setid_doc(String value){ idDocumento = value;}
+	public void setIdioma(String value){ idioma= value;}
+	public void setderechosDeAutor(String value){derechosDeAutor= value;}
+	public void setDescripcion(String value){descripcion= value;}
+	public void setSoftware_recomentado(String value){softwareRecomendado= value;}
+	public void setResolucion(String value){resolucion= value;}
+	public void setEditorial(String value){editorial= value;}
+	public void setFormato(String value){formato= value;}
+	public void settituloppal(String value){tituloPrincipal= value;}
+	public void setTitulo_secundario(String value){tituloSecundario= value;}
+	public void seturl(String value){link= value;}
+	public void setFecha_creacion(String value){fechaCreacion= value;}
+	public void setFecha_publicacion(String value){fechaPublicacion= value;}
+	public void setfechaDeCatalogacion(String value){fechaCatalogacion= value;}	
+	public void setcatalogadorLogin(String value){loginCatalogador= value;}
+	public void setTipoMaterial(String value){tipoMaterial= value;}
+	public void setAutores(Vector<Autor> value){autores= value;}
+	public void setAreas(Vector<AreaConocimiento> value){areas= value;}
+	public void setPalabrasClave(Vector<PalabraClave> value){palabrasClave= value;}
 
 }
