b6d4945d366b0029b28fe0f04bdeaf289965e9b3
yerminson <yermigon@gmail.com>
2011-05-09 19:15:13 -0500

    ingresar palabra clave verificado espacios y longuitud de string

--- BibliotecaDigital/src/GestionDocumento/Gui/GuiIngresarPalabraClave.java
+++ BibliotecaDigital/src/GestionDocumento/Gui/GuiIngresarPalabraClave.java
@@ -7,6 +7,8 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -76,8 +78,12 @@ public class GuiIngresarPalabraClave extends JFrame {
 	}
 
 	private void iniciarCampos() {
+		ManejadorJTextField manejador  = new ManejadorJTextField();
+		
 		campoNombre = new JTextField(15);
+		campoNombre.addKeyListener(manejador);
 		campoDescripcion = new JTextArea(5, 20);
+		campoDescripcion.addKeyListener(manejador);
 		botonIngresarPalabra = new JButton("Registrar Palabra");
 		botonIngresarPalabra.addActionListener(new ManejadorBoton());
 
@@ -105,6 +111,7 @@ public class GuiIngresarPalabraClave extends JFrame {
 		
 		public void actionPerformed(ActionEvent arg0) {
 			
+			
 			if(validarDatos()){
 			
 				ControladorPalabraClave conPalabra = new ControladorPalabraClave();
@@ -112,6 +119,53 @@ public class GuiIngresarPalabraClave extends JFrame {
 				dispose();
 			}
 		}		
+	}
+	
+	private class ManejadorJTextField implements KeyListener{
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+			if( campoNombre == e.getSource()){
+				
+				if(campoNombre.getText().length()>20)
+				{
+					if(e.getKeyCode()!=KeyEvent.VK_BACK_SPACE){
+						getToolkit().beep();//sonido
+						campoNombre.setText(campoNombre.getText().substring(0,19));
+					}
+				}
+			}
+			
+			if(e.getSource()== campoDescripcion){
+				if(campoDescripcion.getText().length()>200)
+				{
+
+					if(e.getKeyCode()!=KeyEvent.VK_BACK_SPACE){
+						getToolkit().beep();//sonido
+						campoDescripcion.setText(campoDescripcion.getText().substring(0,199));
+					}
+				}					
+			}
+			
+			
+
+		
+			
+				
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
 	}
 
 
