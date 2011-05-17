2f9fcff8d05e1167c81e626a01e61d0737c15d3b
yerminson <yermigon@gmail.com>
2011-05-16 19:42:15 -0500

    añadido cambiar contraseña para las personas olvidadizas

--- BibliotecaDigital/src/Usuarios/Gui/GuiRecuperarPassword.java
+++ BibliotecaDigital/src/Usuarios/Gui/GuiRecuperarPassword.java
@@ -1,9 +1,9 @@
 package Usuarios.Gui;
+
 /*
  * Autor Yerminson Gonzalez
  * 
  * */
-
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -25,84 +25,75 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
-
 import Principal.Gui.GuiPrincipal;
 import Usuarios.Controlador.ControladorUsuario;
 import Usuarios.Logica.Usuario;
 
-public class GuiRecuperarPassword extends JPanel
-{
+public class GuiRecuperarPassword extends JPanel {
 
 	private static final long serialVersionUID = 1L;
 	private JLabel nombreUsuario;
 	private JTextField campoNombre;
-	
+
 	private JLabel preguntaSecreta;
 	private JTextField campoDescripcionPregunta;
 	private JLabel respuestaPregunta;
 	private JTextField campoRespuestaPregunta;
-	
+
 	private JLabel password;
 	private JPasswordField campoPassword;
 	private JLabel verificarPassword;
 	private JPasswordField campoVerificarPassword;
-	
-	
+
 	JPanel panelBotonSolicitud;
 	JPanel panelBotonCambiar;
-	
-	JPanel panelBotonGuardar;
-	
 
-	
+	JPanel panelBotonGuardar;
+
 	JPanel panelConsultarLogin;
 	JPanel panelDatosInterno;
-	
+
 	JPanel panelPreguntaSecreta;
 	JPanel panelDatosInterno2;
-	
+
 	JPanel panelPassword;
 	JPanel panelDatosInterno3;
-	
+
 	JPanel panelGeneral;
-	
-	
+
 	private JButton botonIngresar;
 	private JButton botonRegresar;
 	private JButton botonCambiarPassword;
 	private JButton botonGuardar;
 	private GuiPrincipal gp;
-	
-	public GuiRecuperarPassword(GuiPrincipal gp)
-	{
+
+	public GuiRecuperarPassword(GuiPrincipal gp) {
 		this.gp = gp;
 		this.initComponents();
 	}
-	
-	private void initComponents()
-	{
+
+	private void initComponents() {
 		panelBotonSolicitud = new JPanel();
 		panelBotonCambiar = new JPanel();
 		panelBotonGuardar = new JPanel();
 		panelDatosInterno = new JPanel(new GridBagLayout());
 		panelDatosInterno2 = new JPanel(new GridBagLayout());
-		panelDatosInterno3 =  new JPanel(new GridBagLayout());
-	   
-	    
+		panelDatosInterno3 = new JPanel(new GridBagLayout());
+
 		// --------------------------------------------------------
-		//panelDatosInterno.setBorder(BorderFactory.createLineBorder(Color.black));
-		//panelDatosInterno2.setBorder(BorderFactory.createLineBorder(Color.black));
-		//panelBoton.setBorder(BorderFactory.createLineBorder(Color.black));
+		// panelDatosInterno.setBorder(BorderFactory.createLineBorder(Color.black));
+		// panelDatosInterno2.setBorder(BorderFactory.createLineBorder(Color.black));
+		// panelBoton.setBorder(BorderFactory.createLineBorder(Color.black));
 		// ---------------------------------------------------------
 		iniciarLabels();
 		// ---------------------------------------------------------
 		iniciarCampos();
 		// ----------------------------------------------------------
-		
+
 		String title = "::Recuperar Password::";
 
-		Color colorTitulo = new Color(0,50,0);
-		Font fontTitulo = new Font("Book Antiqua",Font.BOLD+ Font.ITALIC, 25);
+		Color colorTitulo = new Color(0, 50, 0);
+		Font fontTitulo = new Font("Book Antiqua", Font.BOLD + Font.ITALIC, 25);
 		// Linea y titulo del panel.
 		TitledBorder borde;
 		borde = BorderFactory.createTitledBorder(BorderFactory
@@ -110,8 +101,6 @@ public class GuiRecuperarPassword extends JPanel
 		borde.setTitleColor(colorTitulo);
 		borde.setTitleFont(fontTitulo);
 		borde.setTitleJustification(TitledBorder.LEFT);
-		
-		
 
 		GridBagConstraints restriccionCampo = new GridBagConstraints(), restriccionEtiquetas = new GridBagConstraints();
 
@@ -129,10 +118,10 @@ public class GuiRecuperarPassword extends JPanel
 		restriccionCampo.anchor = GridBagConstraints.EAST;
 
 		panelDatosInterno.add(nombreUsuario, restriccionEtiquetas);
-	    panelDatosInterno.add(campoNombre, restriccionCampo);
-	    restriccionEtiquetas.gridy = 1;
+		panelDatosInterno.add(campoNombre, restriccionCampo);
+		restriccionEtiquetas.gridy = 1;
 		restriccionCampo.gridy = 1;
-		
+
 		panelDatosInterno2.add(preguntaSecreta, restriccionEtiquetas);
 		panelDatosInterno2.add(campoDescripcionPregunta, restriccionCampo);
 		restriccionEtiquetas.gridy = 2;
@@ -149,8 +138,6 @@ public class GuiRecuperarPassword extends JPanel
 		panelDatosInterno3.add(verificarPassword, restriccionEtiquetas);
 		panelDatosInterno3.add(campoVerificarPassword, restriccionCampo);
 
-		
-
 		panelBotonSolicitud.add(botonIngresar);
 		panelBotonSolicitud.add(botonRegresar);
 		panelBotonCambiar.add(botonCambiarPassword);
@@ -159,56 +146,53 @@ public class GuiRecuperarPassword extends JPanel
 		panelConsultarLogin.setLayout(new BorderLayout());
 		panelConsultarLogin.add(panelDatosInterno, BorderLayout.CENTER);
 		panelConsultarLogin.add(panelBotonSolicitud, BorderLayout.SOUTH);
-		
-		
+
 		panelPreguntaSecreta = new JPanel();
-		
+
 		panelPreguntaSecreta.setLayout(new BorderLayout());
 		panelPreguntaSecreta.add(panelDatosInterno2, BorderLayout.CENTER);
-		panelPreguntaSecreta.add(panelBotonCambiar,BorderLayout.SOUTH);
-		
-		panelPassword  = new JPanel();
+		panelPreguntaSecreta.add(panelBotonCambiar, BorderLayout.SOUTH);
+
+		panelPassword = new JPanel();
 		panelPassword.setLayout(new BorderLayout());
 		panelPassword.add(panelDatosInterno3, BorderLayout.CENTER);
-		panelPassword.add(panelBotonGuardar,BorderLayout.SOUTH);
-		
+		panelPassword.add(panelBotonGuardar, BorderLayout.SOUTH);
+
 		panelGeneral = new JPanel(new BorderLayout());
 		panelGeneral.setBorder(borde);
-		panelGeneral.add(panelConsultarLogin,BorderLayout.NORTH);
-		panelGeneral.add(panelPreguntaSecreta,BorderLayout.CENTER);
-		panelGeneral.add(panelPassword,BorderLayout.SOUTH);
-		
-		
+		panelGeneral.add(panelConsultarLogin, BorderLayout.NORTH);
+		panelGeneral.add(panelPreguntaSecreta, BorderLayout.CENTER);
+		panelGeneral.add(panelPassword, BorderLayout.SOUTH);
+
 		panelPreguntaSecreta.setVisible(false);
 		panelBotonCambiar.setVisible(false);
 		panelPassword.setVisible(false);
 		panelBotonGuardar.setVisible(false);
 
 		this.add(panelGeneral);
-		
+
 		this.setSize(430, 220);
 		this.setVisible(true);
 	}
-	
-	private void iniciarCampos()
-	{
+
+	private void iniciarCampos() {
 		campoNombre = new JTextField(20);
 		campoNombre.addKeyListener(new ManejadorJTexField());
-			
+
 		campoRespuestaPregunta = new JTextField(20);
-		
+
 		campoDescripcionPregunta = new JTextField(20);
 		campoDescripcionPregunta.setEditable(false);
-		
+
 		campoPassword = new JPasswordField(20);
 		campoVerificarPassword = new JPasswordField(20);
-		
+
 		ManejadorBoton manejador = new ManejadorBoton();
-		
+
 		botonIngresar = new JButton("Ingresar al Sistema");
 		botonIngresar.addActionListener(manejador);
 		botonCambiarPassword = new JButton("Cambiar Contraseña");
-	
+
 		botonCambiarPassword.addActionListener(manejador);
 		botonGuardar = new JButton("Guardar");
 		botonGuardar.addActionListener(manejador);
@@ -217,160 +201,155 @@ public class GuiRecuperarPassword extends JPanel
 
 	}
 
-	private void iniciarLabels()
-	{
+	private void iniciarLabels() {
 		Font font1 = new Font("Book Antiqua", Font.BOLD + Font.ITALIC, 17);
 		Font font3 = new Font("Book Antiqua", Font.BOLD + Font.ITALIC, 25);
 
 		Color colorletras = new Color(0, 60, 0);
 
-		
-		nombreUsuario = new JLabel("Login  :");	
-		nombreUsuario.setFont(font1);	
+		nombreUsuario = new JLabel("Login  :");
+		nombreUsuario.setFont(font1);
 		nombreUsuario.setForeground(colorletras);
-		
+
 		preguntaSecreta = new JLabel("Pregunta Secreta: ");
-		preguntaSecreta.setFont(font1);	
+		preguntaSecreta.setFont(font1);
 		preguntaSecreta.setForeground(colorletras);
 		respuestaPregunta = new JLabel("Respuesta: ");
-		respuestaPregunta.setFont(font1);	
+		respuestaPregunta.setFont(font1);
 		respuestaPregunta.setForeground(colorletras);
-		
+
 		password = new JLabel("Password: ");
-		password.setFont(font1);	
+		password.setFont(font1);
 		password.setForeground(colorletras);
 		verificarPassword = new JLabel("Verifique el Password: ");
-		verificarPassword.setFont(font1);	
+		verificarPassword.setFont(font1);
 		verificarPassword.setForeground(colorletras);
-		
+
 	}
-	
-	private class ManejadorBoton implements ActionListener
-	{
+
+	private class ManejadorBoton implements ActionListener {
 
 		Usuario usuario = null;
 		ControladorUsuario controladorUsuario;
+
 		@Override
-		public void actionPerformed(ActionEvent evento)
-		{
-			if(evento.getSource() == botonIngresar)
-			{
+		public void actionPerformed(ActionEvent evento) {
+			if (evento.getSource() == botonIngresar) {
 				controladorUsuario = new ControladorUsuario();
 				String login = campoNombre.getText();
-				usuario  = controladorUsuario.consultarUsuario(login);
+				usuario = controladorUsuario.consultarUsuario(login);
 				String nombre = usuario.getNombre1();
-				if(nombre == null )
-				{
-					
-					JOptionPane.showMessageDialog(null,"Login incorrecto por favor intente nuevamente");
-					
-				}else
-				{
+				if (nombre == null) {
+
+					JOptionPane.showMessageDialog(null,
+							"Login incorrecto por favor intente nuevamente");
+
+				} else {
 					panelConsultarLogin.setVisible(false);
 					panelBotonSolicitud.setVisible(false);
+					panelBotonSolicitud.remove(botonRegresar);
 					panelPreguntaSecreta.setVisible(true);
+					panelBotonCambiar.add(botonRegresar);
 					panelBotonCambiar.setVisible(true);
-					campoDescripcionPregunta.setText(usuario.getPreguntaSecreta());
 					
+					campoDescripcionPregunta.setText(usuario
+							.getPreguntaSecreta());
+
 				}
-				
-			
-					
-			}else if(evento.getSource() == botonCambiarPassword)
-			{
+
+			} else if (evento.getSource() == botonCambiarPassword) {
 				String respuestaDada = campoRespuestaPregunta.getText();
 				String respuestaCorrecta = usuario.getRespuestaSecreta();
-				System.out.println(usuario.getContrasena());
-				System.out.println(respuestaCorrecta);
-				if(respuestaDada.equals(respuestaCorrecta))
-				{
+				System.out.println("ENTRE AYUDA");
+				
+				if (respuestaDada.equals(respuestaCorrecta)) {
 					panelPreguntaSecreta.setVisible(false);
 					panelBotonCambiar.setVisible(false);
+					panelBotonCambiar.remove(botonRegresar);
 					panelPassword.setVisible(true);
+					panelBotonGuardar.add(botonRegresar);
 					panelBotonGuardar.setVisible(true);
-					
-				}else
-				{
-					JOptionPane.showMessageDialog(null,"No es la respuesta adecuada por favor intente nuevamente");
-					
+
+				} else {
+					JOptionPane
+							.showMessageDialog(null,
+									"No es la respuesta adecuada por favor intente nuevamente");
+
 				}
-				
-				
-			}else if(evento.getSource() == botonGuardar)
-			{
+
+			} else if (evento.getSource() == botonGuardar) {
 				String password = new String(campoPassword.getPassword());
-				String verificacionPassword =new String(campoVerificarPassword.getPassword());
+				String verificacionPassword = new String(campoVerificarPassword
+						.getPassword());
 				System.out.println(password);
-				if(password.equals(verificacionPassword))
-				{
+				if (password.equals(verificacionPassword)) {
 					usuario.setContrasena(password);
 					controladorUsuario.modificarUsuario(usuario);
-					JOptionPane.showMessageDialog(null,"Se cambio correctamente la contraseña ahora puede ingresar");
+					JOptionPane
+							.showMessageDialog(null,
+									"Se cambio correctamente la contraseña ahora puede ingresar");
 					System.out.println("cambiando panel");
 					GuiPrincipal.cambiarPanelIngresarRemover();
 					gp.repaint();
 					restaurar();
-					
-					
+
+				} else {
+					JOptionPane
+							.showMessageDialog(null,
+									"Las contraseñas no coinciden por favor intente nuevamente");
 				}
-				else
-				{
-					JOptionPane.showMessageDialog(null,"Las contraseñas no coinciden por favor intente nuevamente");
-				}
+
+			} else if (evento.getSource() == botonRegresar) {
+
 				
-				
-			}else if(evento.getSource() == botonRegresar)
-			{
-				
-					GuiPrincipal.cambiarPanelIngresarRemover();
-					gp.repaint();
-					
-			
-				
-				
-			}				
-		}	
+				GuiPrincipal.cambiarPanelIngresarRemover();
+				restaurar();
+				gp.repaint();
+
+			}
+		}
 	}
-	
-	private class ManejadorJTexField implements KeyListener{
+
+	private class ManejadorJTexField implements KeyListener {
 
 		@Override
 		public void keyPressed(KeyEvent e) {
-			if(e.getKeyCode() == KeyEvent.VK_ENTER)
-				botonIngresar.doClick();			
+			if (e.getKeyCode() == KeyEvent.VK_ENTER)
+				botonIngresar.doClick();
 		}
 
 		@Override
 		public void keyReleased(KeyEvent arg0) {
 			// TODO Auto-generated method stub
-			
+
 		}
 
 		@Override
 		public void keyTyped(KeyEvent arg0) {
 			// TODO Auto-generated method stub
-			
+
 		}
-		
-	}
-	
-	
-	private void restaurar()
-	{
-		panelConsultarLogin.setVisible(true);
-		panelBotonSolicitud.setVisible(true);
-	
-		campoNombre.setText(""); 
-		campoDescripcionPregunta.setText("");
-		campoRespuestaPregunta.setText("");
-		
-		campoPassword.setText("");
-		campoVerificarPassword.setText("");
-		
-		panelPassword.setVisible(false);
-		panelBotonGuardar.setVisible(false);
-		
+
 	}
 
+	private void restaurar() {
+		panelConsultarLogin.setVisible(true);
+		panelBotonSolicitud.add(botonRegresar);
+		panelBotonSolicitud.setVisible(true);
+
+		campoNombre.setText("");
+		campoDescripcionPregunta.setText("");
+		campoRespuestaPregunta.setText("");
+
+		campoPassword.setText("");
+		campoVerificarPassword.setText("");
+
+		panelPassword.setVisible(false);
+		panelBotonGuardar.setVisible(false);
+		panelPreguntaSecreta.setVisible(false);
+		panelBotonCambiar.setVisible(false);
+		
+
+	}
 
 }
