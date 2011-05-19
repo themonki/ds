dd0710f62656e8d67fb6c662be001edec2ac244e
Cristian Leonardo Rios Lopez <dragonblanco452@gmail.com>
2011-05-18 22:46:55 -0500

    agregado consulta general

--- BibliotecaDigital/src/Consultas/Dao/DaoConsulta.java
+++ BibliotecaDigital/src/Consultas/Dao/DaoConsulta.java
@@ -19,28 +19,151 @@ public class DaoConsulta {
 	
 	
 	//metodo de consulta general
-	public Vector<Consulta> consultaGeneral(Vector<String> parametro){
+	public Vector<Consulta> consultaGeneral(Vector<String> parametro)
+	{
+		Vector<Consulta> consultas = new Vector<Consulta>();
+		String consultaSql, consultaDocumentoSql, consultaPalabraSql, consultaAreaSql, consultaAutorSql;
 		
-		Vector<Consulta> vCon = new Vector<Consulta>();
+		consultaDocumentoSql = "SELECT d.id_documento, d.titulo_principal "+
+		"FROM Documento AS d " +
+		"WHERE ";
 		
-		/*String consulta_sql = "SELECT " +
-				"FROM " +
-				"WHERE ;";
+		for(int i=0; i<parametro.size();i++)
+		{
+			consultaDocumentoSql += "d.descripcion LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		//consultaDocumentoSql += "d.descripcion LIKE '%" + parametro.lastElement() + "%'";
+		
+		for(int i=0; i<parametro.size();i++)
+		{
+			consultaDocumentoSql += "d.titulo_principal LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		//consultaDocumentoSql += "d.titulo_principal LIKE '%" + parametro.lastElement() + "%'";
+	
+		for(int i=0; i<parametro.size()-1;i++)
+		{
+			consultaDocumentoSql += "d.titulo_secundario LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		consultaDocumentoSql += "d.titulo_secundario LIKE '%" + parametro.lastElement() + "%'";
+		
+		consultaPalabraSql = "SELECT * FROM " +
+				"(SELECT d.id_documento, d.titulo_principal FROM Documento AS d) AS f " +
+				"NATURAL JOIN " +
+				"(SELECT t.id_documento FROM tiene_documento_palabra_clave AS t " +
+				"NATURAL JOIN " +
+				"(SELECT p.nombre FROM palabra_clave AS p WHERE " ;		
+		
+		for(int i=0; i<parametro.size();i++)
+		{
+			consultaPalabraSql += "p.nombre LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		
+		for(int i=0; i<parametro.size()-1;i++)
+		{
+			consultaPalabraSql += "p.descripcion LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		consultaPalabraSql += "p.descripcion LIKE '%" + parametro.lastElement() + "%') AS c) AS e";
+		
+		consultaAreaSql = "SELECT * FROM " +
+				"(SELECT d.id_documento, d.titulo_principal FROM documento AS d) AS f " +
+				"NATURAL JOIN " + 
+				"(SELECT p.id_documento FROM pertenece_documento_area_conocimiento AS p " +
+				"NATURAL JOIN " +
+				"(SELECT a.id_area FROM area_conocimiento AS a WHERE ";
+		
+		for(int i=0; i<parametro.size();i++)
+		{
+			consultaAreaSql += "a.nombre LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		
+		for(int i=0; i<parametro.size()-1;i++)
+		{
+			consultaAreaSql += "a.descripcion LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		consultaAreaSql += "a.descripcion LIKE '%" + parametro.lastElement() + "%') AS c) AS e";
+		
+		consultaAutorSql = "SELECT * FROM " +
+		"(SELECT d.id_documento, d.titulo_principal FROM documento AS d) AS x " +
+		"NATURAL JOIN " + 
+		"(SELECT e.id_documento FROM escribe_autor_documento AS e " + 
+		"NATURAL JOIN " + 
+		"(SELECT a.id_autor FROM autor AS a WHERE ";
+		
+		for(int i=0;i<parametro.size();i++)
+		{
+			consultaAutorSql += "a.nombre LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		
+		for(int i=0;i<parametro.size()-1;i++)
+		{
+			consultaAutorSql += "a.apellido LIKE '%" + parametro.elementAt(i) + "%' OR ";
+		}
+		consultaAutorSql += "a.apellido LIKE '%" + parametro.lastElement() + "%') AS c) AS f";
+
+		//System.out.println(consultaDocumentoSql);
+		//System.out.println(consultaPalabraSql);
+		//System.out.println(consultaAreaSql);
+		//System.out.println(consultaAutorSql);
+		
+		consultaSql = "SELECT * FROM " +
+		"((" + consultaDocumentoSql + " UNION " + consultaPalabraSql + ") UNION " +
+		"(" + consultaAreaSql + " UNION " + consultaAutorSql + ")) AS y";
+		
+		//System.out.println(consultaSql);
+		
 		ResultSet resultado;		
-		//System.out.println(consulta_sql);
+
 		try {
 			Connection conn = fachada.conectar();
 			Statement sentencia = conn.createStatement();			
-			resultado = sentencia.executeQuery(consulta_sql);
-			while (resultado.next()) {
+			resultado = sentencia.executeQuery(consultaSql);
+			while (resultado.next())
+			{
+				Consulta consulta = new Consulta();
 				
+				consulta.setIdDocumento(resultado.getString("id_documento"));
+				consulta.setTituloDocuemto(resultado.getString("titulo_principal"));
+				consulta.setNombresAutoresDocumento(consultarAutoresDocumento(resultado.getString("id_documento")));
+				consultas.add(consulta);
 			}
 			conn.close();			
 			} catch (SQLException e) {			
 				System.out.println(e);
 			} catch (Exception e) {
 				System.out.println(e);					
-			}*/
-		return vCon;
+			}
+			System.out.println(consultas);
+		return consultas;
+	}
+	
+	Vector<String> consultarAutoresDocumento(String idDocumento)
+	{
+		Vector<String> autores = new Vector<String>();
+		
+		String consultaSql;
+		consultaSql = "SELECT y.nombre FROM " +
+				"(SELECT a.id_autor, a.nombre FROM autor AS a) AS y NATURAL JOIN " +
+				"(SELECT d.id_autor FROM escribe_autor_documento AS d WHERE d.id_documento = " + idDocumento + ") AS x";
+		
+		ResultSet tabla;		
+
+		try
+		{
+			Connection conn = fachada.conectar();
+			Statement sentencia = conn.createStatement();			
+			tabla = sentencia.executeQuery(consultaSql);
+		
+			while (tabla.next())
+			{
+				autores.add(tabla.getString("nombre"));
+			}
+			conn.close();			
+			} catch (SQLException e) {			
+				System.out.println(e);
+			} catch (Exception e) {
+				System.out.println(e);					
+			}
+					
+		return autores;
 	}
 }
