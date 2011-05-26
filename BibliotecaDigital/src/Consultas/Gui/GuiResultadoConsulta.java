e2db28181d4483a14cf90c2dbaefd96c33de8c6f
themonki <edgarandres29@gmail.com>
2011-05-26 16:58:33 -0500

    adicionando el atributo a la clase usuario de fecha de ultimo acceso

--- BibliotecaDigital/src/Consultas/Gui/GuiResultadoConsulta.java
+++ BibliotecaDigital/src/Consultas/Gui/GuiResultadoConsulta.java
@@ -44,6 +44,7 @@ public class GuiResultadoConsulta extends JScrollPane{
 	public static int TIPOCONSULTA;
 	RenderLista rl;//formato de la lista
 	TitledBorder borde;
+	boolean es_novedad;
 	
 	public GuiResultadoConsulta(){}
 	
@@ -52,9 +53,12 @@ public class GuiResultadoConsulta extends JScrollPane{
 		cantidadMostrar=cantidadResultados;
 		this.vectorConsulta = vectorConsulta;
 		cantidadTotalResultados=vectorConsulta.size();
-		posicionResultado=0;		
+		posicionResultado=0;
+		es_novedad = false;
 		iniComponents();		
 	}
+	
+	public void setEs_novedad(boolean value){es_novedad=value;}
 	
 	private void iniComponents(){
 		//***/
@@ -234,6 +238,16 @@ public class GuiResultadoConsulta extends JScrollPane{
 					conConsulta.insertarConsultaDocumentoUsuario(d.getId_doc(),
 							GuiAdministrador.LOGIN);
 				}
+				
+				if(es_novedad){
+					
+					/*
+					 * aca lo necesario para mostrar vista documento
+					 * */
+					
+					return;
+				}
+				
 				if (GuiResultadoConsulta.TIPOCONSULTA == 1) {
 
 					GuiConsultaBasica.vistaDocumento = new GuiVistaDocumento(d);
