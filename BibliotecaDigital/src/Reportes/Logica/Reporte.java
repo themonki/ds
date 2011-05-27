c7ec3dd9635afdd29037ea7e86d89a251ddf364e
andrea08 <andrea1391@gmail.com>
2011-05-27 08:23:30 -0500

    clase reporte ..

--- BibliotecaDigital/src/Reportes/Logica/Reporte.java
+++ BibliotecaDigital/src/Reportes/Logica/Reporte.java
@@ -7,5 +7,47 @@ public class Reporte {
 	private Vector<String> vectorOrderBy;
 	private Vector<String> vectorColumnas;
 	private Vector<String> vectorRegistros;
+	private int tipo; //Tipo orderby, tipo condicion.
+	private String ruta;
+	
+	public Reporte()
+	{
+		
+	}
 
+	public Reporte(Vector<String> vectorOrderBy, Vector<String> vectorColumnas, Vector<String> vectorRegistros, int tipo, String ruta)
+	{
+		this.ruta = ruta;
+		this.tipo = tipo;
+		this.vectorColumnas = vectorColumnas;
+		this.vectorOrderBy = vectorOrderBy;
+		this.vectorRegistros = vectorRegistros;
+		
+	}
+	
+	public Vector<String> getVectorColumnas()
+	{
+		return this.vectorColumnas;
+	}
+	
+	public Vector<String> getVectorOrderBy()
+	{
+		return this.vectorOrderBy;
+	}
+	
+	public Vector<String> getVectorRegistros()
+	{
+		return this.vectorRegistros;
+	}
+	
+	public String getRuta()
+	{
+		return this.ruta;
+	}
+	
+	public int getTipo()
+	{
+		return this.tipo;
+	}
 }
+
