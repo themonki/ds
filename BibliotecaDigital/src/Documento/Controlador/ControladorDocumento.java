2666a12948582e9021aaf98d8550f21242883996
themonki <edgarandres29@gmail.com>
2011-06-05 01:46:48 -0500

    acomodando datos al catalogar y modificar documento

--- BibliotecaDigital/src/Documento/Controlador/ControladorDocumento.java
+++ BibliotecaDigital/src/Documento/Controlador/ControladorDocumento.java
@@ -106,9 +106,8 @@ public class ControladorDocumento {
 		if(d.getResolucion().equals("")){
 			d.setResolucion("sin resolucion");
 		}
-		
-		if(d.getTitulo_secundario().equals("")){
-			d.setTitulo_secundario("sin titulo secundario");
+		if(d.getSoftware_recomentado().equals("")){
+			d.setSoftware_recomentado("ninguno");
 		}
 		DaoDocumento daoDoc = new DaoDocumento();
 		int value = daoDoc.guardarDocumento(d);
@@ -188,7 +187,12 @@ public class ControladorDocumento {
 		d.setTituloppal(d.getTituloppal().toLowerCase());
 		d.setTitulo_secundario(d.getTitulo_secundario().toLowerCase());
 		d.setTipoMaterial(d.getTipoMaterial().toLowerCase());
-		
+		if(d.getResolucion().equals("")){
+			d.setResolucion("sin resolucion");
+		}
+		if(d.getSoftware_recomentado().equals("")){
+			d.setSoftware_recomentado("ninguno");
+		}
 		DaoDocumento daoDoc = new DaoDocumento();
 		int value = daoDoc.modificarDocumento(d);
 		System.out.println("Se modifico el documento");
